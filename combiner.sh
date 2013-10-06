d36909f7968cbc1f480e0d1f103f8327397dde6d
Brad Larson <brad@shopsimply.me>
2013-10-06 16:01:10 -0400

    Updating Mental Models

--- combiner.sh
+++ combiner.sh
@@ -8,3 +8,4 @@ while read p; do
 	NAME="${p%%.*}"
 	if [ "$NAME" != "header" ]; then if [ "$NAME" != "mental_models" ]; then if [ "$NAME" != "README" ]; then cat section_break.txt $p >> $DEST; fi; fi; fi
 done < files.txt
+rm files.txt
