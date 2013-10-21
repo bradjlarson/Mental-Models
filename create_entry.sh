020268396c9b770eeef5a7685ba5d797c6a871f5
Brad Larson <brad@shopsimply.me>
2013-10-20 21:01:56 -0400

    Updating Mental Models

--- create_entry.sh
+++ create_entry.sh
@@ -2,7 +2,7 @@
 echo -n "What is the model name?";read modelname;
 file_name="$modelname.md"
 export modelname
-cp _blank.md $file_name
-perl -pi -e 's/Model Name/$ENV{modelname}/g' $file_name
-#sed -e 's/Model Name/$ENV{modelname}/g' $filename
-vi $file_name
+cp _blank.md models/$file_name
+perl -pi -e 's/Model Name/$ENV{modelname}/g' models/$file_name
+#sed -e 's/Model Name/$ENV{modelname}/g' models/$filename
+vi models/$file_name
---
+++ models/critical_mass.md
@@ -0,0 +1,17 @@
+####Critical Mass:
+#####Summary:
+
+Critical Mass relates to when a reaction has achieved self sustaining momentum/fuel, allowing the reaction to continue without inputting additional energy. 
+
+#####Traditional Usage:
+
+The term is commonly used in chemistry, particularly with nuclear reactions. 
+
+#####How it works:
+
+#####Edge Cases:
+
+#####Additional Information:
+
+#####Other Model Interactions:
+
